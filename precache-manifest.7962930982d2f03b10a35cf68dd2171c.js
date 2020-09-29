be160e143bf6b4bcaf99ea7441513b390f6c3549
aprillebalsom <aprillebalsom@gmail.com>
2020-09-29 09:13:35 -0400

    Updates

---
+++ precache-manifest.7962930982d2f03b10a35cf68dd2171c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e58e4cc0914567388a684ee0b71bca",
+    "url": "/aprille-balsom-project-5/index.html"
+  },
+  {
+    "revision": "646edc3ba1e6494ed39e",
+    "url": "/aprille-balsom-project-5/static/css/main.be4d3217.chunk.css"
+  },
+  {
+    "revision": "e557d6f14e9e5693a433",
+    "url": "/aprille-balsom-project-5/static/js/2.b8b4862e.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/aprille-balsom-project-5/static/js/2.b8b4862e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "646edc3ba1e6494ed39e",
+    "url": "/aprille-balsom-project-5/static/js/main.d850ce24.chunk.js"
+  },
+  {
+    "revision": "3524436fd0cf7f0cb2ff",
+    "url": "/aprille-balsom-project-5/static/js/runtime-main.c0f96701.js"
+  }
+]);
